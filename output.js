caaf133cbe4cafdeddc2aab7e2dc9cac33060006
action <action@github.com>
2024-10-20 04:42:32 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,7 +1,15 @@
-//Sun Oct 20 2024 04:37:01 GMT+0000 (Coordinated Universal Time)
+//Sun Oct 20 2024 04:42:32 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
-(function () {})();
+var obj = {
+    "code": "2202",
+    "camera_config": "null",
+    "upload_url": "https://www.argun.cc/",
+    "ref_id": "6506-0009487784-argun666niuniujun0fdcnchgqz7s6",
+    "smile_job_id": "6666666666"
+  },
+  body = JSON.stringify(obj);
 $done({
+  "body": body,
   "status": 200
 });
